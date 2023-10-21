1a38f82d23c3d0d2ca3d1a3509a427a2747a51d8
Jack Chan <JackChanHW@connect.hku.hk>
2023-10-21 15:09:18 +0100

    cli for alter cbz thumbnail

---
+++ thumbnail/crop.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+from wand.image import Image
+import argparse
+import shutil
+import tempfile
+from math import floor
+from pathlib import Path
+import zipfile
+
+
+def main():
+    # get input file path
+    parser = argparse.ArgumentParser(description="crop images left or right")
+    parser.add_argument("-i", "--input", required=True, help="input file")
+    parser.add_argument(
+        "-r",
+        "--right",
+        action="store_true",
+        help="crop the right side of the thumbnail",
+    )
+    args = parser.parse_args()
+    inputFilePath = Path(args.input)
+
+    with tempfile.TemporaryDirectory() as tempDir:
+        unzipDirPath = Path(tempDir)
+        # unzip file
+        shutil.unpack_archive(inputFilePath, unzipDirPath, "zip")
+
+        # get first fileName
+        files = [f for f in unzipDirPath.iterdir() if f.is_file()]
+        files.sort()
+        prevImagePath = Path(files[0])
+
+        newImageFilePath = prevImagePath.parent / ("_0" + prevImagePath.suffix)
+        if newImageFilePath.exists():
+            raise FileExistsError(newImageFilePath.name + " exist.")
+
+        # crop thumbnail
+        with Image(filename=prevImagePath) as img:
+            (w, _) = img.size
+            with img.clone() as i:
+                newW = floor(w / 2)
+                if args.right:
+                    i.crop(newW, 0, width=newW)
+                else:
+                    i.crop(0, 0, right=newW)
+                i.save(filename=newImageFilePath)
+        files.append(newImageFilePath)
+
+        # zip file
+        with zipfile.ZipFile(inputFilePath, "w") as zf:
+            for file in files:
+                zf.write(file, arcname=file.name)
+
+
+if __name__ == "__main__":
+    main()
---
+++ thumbnail/select-page.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+from wand.image import Image
+from wand.display import display
+import argparse
+import shutil
+import tempfile
+import zipfile
+from pathlib import Path
+
+
+def main():
+    # get input file path
+    parser = argparse.ArgumentParser(description="select page as thumbnail")
+    parser.add_argument("-i", "--input", required=True, help="input file")
+    parser.add_argument(
+        "-p", "--page", type=int, required=True, help="page number to be thumbnail"
+    )
+    parser.add_argument(
+        "-d",
+        "--display",
+        action="store_true",
+        help="display preview and ask for confirm",
+    )
+    args = parser.parse_args()
+    inputFilePath = Path(args.input)
+
+    with tempfile.TemporaryDirectory() as tempDir:
+        unzipDirPath = Path(tempDir)
+        # unzip file
+        shutil.unpack_archive(inputFilePath, unzipDirPath, "zip")
+
+        # get first fileName
+        files = [f for f in unzipDirPath.iterdir() if f.is_file()]
+        if len(files) < args.page:
+            raise ValueError("selected page doesn't exist")
+        files.sort()
+        imageFileName = files[args.page]
+
+        newImageFilePath = imageFileName.parent / ("_0" + imageFileName.suffix)
+        if newImageFilePath.exists():
+            raise FileExistsError(newImageFilePath.name + " exist.")
+
+        if args.display:
+            with Image(filename=imageFileName) as img:
+                display(img)
+            confirm = input("Confirm? [Y]/N")
+            if confirm == "N" or confirm == "n":
+                exit()
+
+        # zip file
+        with zipfile.ZipFile(inputFilePath, "w") as zf:
+            zf.write(imageFileName, arcname=newImageFilePath.name)
+            for file in files:
+                zf.write(file, arcname=file.name)
+
+
+if __name__ == "__main__":
+    main()
